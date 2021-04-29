11a00058b8cd5c4fdc63269edbfe1507727502e3
Baran <57062330+Xenon67@users.noreply.github.com>
2021-04-29 23:01:37 +0300

    Update heroku.js

--- plugins/heroku.js
+++ plugins/heroku.js
@@ -14,6 +14,7 @@ const {MessageType} = require('@adiwajshing/baileys');
 
 const Language = require('../language');
 const Lang = Language.getString('heroku');
+const Langr = Language.getString('lydia');
 
 const heroku = new Heroku({
     token: Config.HEROKU.API_KEY
@@ -21,6 +22,133 @@ const heroku = new Heroku({
 
 
 let baseURI = '/apps/' + Config.HEROKU.APP_NAME;
+
+Asena.addCommand({pattern: 'degis ?(.*)', fromMe: true, desc: Lang.DEGİS_DESC}, (async (message, match) => {
+
+    if (match[1] == '' && message.reply_message) {
+        return await message.client.sendMessage(message.jid, Lang.DEGİS_NONE, MessageType.text); 
+    }
+    else if (match[1] == ''  && !message.reply_message) {
+        return await message.client.sendMessage(message.jid, Langr.NEED_REPLY, MessageType.text); 
+    }
+    else if (match[1] == 'ban' && message.reply_message) {
+        await message.client.sendMessage(message.jid, Lang.SUCC, MessageType.text);
+        await new Promise(r => setTimeout(r, 1200));
+        await message.client.sendMessage(message.jid, Lang.SUCC_AF, MessageType.text);
+        await heroku.patch(baseURI + '/config-vars', { 
+            body: { 
+                ['BAN_MESSAGE']: message.reply_message.text
+            } 
+        });
+    }
+    else if (match[1] == 'mute' && message.reply_message) {
+        await message.client.sendMessage(message.jid, Lang.SUCC, MessageType.text);
+        await new Promise(r => setTimeout(r, 1200));
+        await message.client.sendMessage(message.jid, Lang.SUCC_AF, MessageType.text);
+        await heroku.patch(baseURI + '/config-vars', { 
+            body: { 
+                ['MUTE_MESSAGE']: message.reply_message.text
+            } 
+        });
+    }
+    else if (match[1] == 'unmute' && message.reply_message) {
+        await message.client.sendMessage(message.jid, Lang.SUCC, MessageType.text);
+        await new Promise(r => setTimeout(r, 1200));
+        await message.client.sendMessage(message.jid, Lang.SUCC_AF, MessageType.text);
+        await heroku.patch(baseURI + '/config-vars', { 
+            body: { 
+                ['UNMUTE_MESSAGE']: message.reply_message.text
+            } 
+        });
+    }
+    else if (match[1] == 'add' && message.reply_message) {
+        await message.client.sendMessage(message.jid, Lang.SUCC, MessageType.text);
+        await new Promise(r => setTimeout(r, 1200));
+        await message.client.sendMessage(message.jid, Lang.SUCC_AF, MessageType.text);
+        await heroku.patch(baseURI + '/config-vars', { 
+            body: { 
+                ['ADD_MESSAGE']: message.reply_message.text
+            } 
+        });
+    }
+    else if (match[1] == 'kickme' && message.reply_message) {
+        await message.client.sendMessage(message.jid, Lang.SUCC, MessageType.text);
+        await new Promise(r => setTimeout(r, 1200));
+        await message.client.sendMessage(message.jid, Lang.SUCC_AF, MessageType.text);
+        await heroku.patch(baseURI + '/config-vars', { 
+            body: { 
+                ['KICKME_MESSAGE']: message.reply_message.text
+            } 
+        });
+    }
+    else if (match[1] == 'afk' && message.reply_message) {
+        await message.client.sendMessage(message.jid, Lang.SUCC, MessageType.text);
+        await new Promise(r => setTimeout(r, 1200));
+        await message.client.sendMessage(message.jid, Lang.SUCC_AF, MessageType.text);
+        await heroku.patch(baseURI + '/config-vars', { 
+            body: { 
+                ['AFK_MESSAGE']: message.reply_message.text
+            } 
+        });
+    }
+    else if (match[1] == 'alive' && message.reply_message) {
+        await message.client.sendMessage(message.jid, Lang.SUCC, MessageType.text);
+        await new Promise(r => setTimeout(r, 1200));
+        await message.client.sendMessage(message.jid, Lang.SUCC_AF, MessageType.text);
+        await heroku.patch(baseURI + '/config-vars', { 
+            body: { 
+                ['ALIVE_MESSAGE']: message.reply_message.text
+            } 
+        });
+    }
+    else if (match[1] == 'demote' && message.reply_message) {
+        await message.client.sendMessage(message.jid, Lang.SUCC, MessageType.text);
+        await new Promise(r => setTimeout(r, 1200));
+        await message.client.sendMessage(message.jid, Lang.SUCC_AF, MessageType.text);
+        await heroku.patch(baseURI + '/config-vars', { 
+            body: { 
+                ['DEMOTE_MESSAGE']: message.reply_message.text
+            } 
+        });
+    }
+    else if (match[1] == 'promote' && message.reply_message) {
+        await message.client.sendMessage(message.jid, Lang.SUCC, MessageType.text);
+        await new Promise(r => setTimeout(r, 1200));
+        await message.client.sendMessage(message.jid, Lang.SUCC_AF, MessageType.text);
+        await heroku.patch(baseURI + '/config-vars', { 
+            body: { 
+                ['PROMOTE_MESSAGE']: message.reply_message.text
+            } 
+        });
+    }
+    else if (match[1] == 'block' && message.reply_message) {
+        await message.client.sendMessage(message.jid, Lang.SUCC, MessageType.text);
+        await new Promise(r => setTimeout(r, 1200));
+        await message.client.sendMessage(message.jid, Lang.SUCC_AF, MessageType.text);
+        await heroku.patch(baseURI + '/config-vars', { 
+            body: { 
+                ['BLOCK_MESSAGE']: message.reply_message.text
+            } 
+        });
+    }
+    else if (match[1] == 'unblock' && message.reply_message) {
+        await message.client.sendMessage(message.jid, Lang.SUCC, MessageType.text);
+        await new Promise(r => setTimeout(r, 1200));
+        await message.client.sendMessage(message.jid, Lang.SUCC_AF, MessageType.text);
+        await heroku.patch(baseURI + '/config-vars', { 
+            body: { 
+                ['UNBLOCK_MESSAGE']: message.reply_message.text
+            } 
+        });
+    }
+    else if ((!match[1] == 'unblock' || !match[1] == 'add' || !match[1] == 'block' || !match[1] == 'mute' || !match[1] == 'unmute' || !match[1] == 'afk' || !match[1] == 'alive' || !match[1] == 'demote' || !match[1] == 'promote' || !match[1] == 'ban' || !match[1] == 'kickme') && message.reply_message) {
+        return await message.client.sendMessage(message.jid, Lang.WR, MessageType.text);
+    }
+    else if ((!match[1] == 'unblock' || !match[1] == 'add' || !match[1] == 'block' || !match[1] == 'mute' || !match[1] == 'unmute' || !match[1] == 'afk' || !match[1] == 'alive' || !match[1] == 'demote' || !match[1] == 'promote' || !match[1] == 'ban' || !match[1] == 'kickme') && !message.reply_message) {
+        return await message.client.sendMessage(message.jid, Lang.WR, MessageType.text);
+    }
+}));
+
 
 Asena.addCommand({pattern: 'restart', fromMe: true, desc: Lang.RESTART_DESC}, (async (message, match) => {
 
@@ -83,6 +211,36 @@ if (Config.WORKTYPE == 'private') {
 else if (Config.WORKTYPE == 'public') {
 
     Asena.addCommand({pattern: 'dyno', fromMe: false, desc: Lang.DYNO_DESC}, (async (message, match) => {
+
+        heroku.get('/account').then(async (account) => {
+            // have encountered some issues while calling this API via heroku-client
+            // so let's do it manually
+            url = "https://api.heroku.com/accounts/" + account.id + "/actions/get-quota"
+            headers = {
+                "User-Agent": "Chrome/80.0.3987.149 Mobile Safari/537.36",
+                "Authorization": "Bearer " + Config.HEROKU.API_KEY,
+                "Accept": "application/vnd.heroku+json; version=3.account-quotas",
+            }
+            await got(url, {headers: headers}).then(async (res) => {
+               const resp = JSON.parse(res.body);
+               total_quota = Math.floor(resp.account_quota);
+               quota_used = Math.floor(resp.quota_used);         
+               percentage = Math.round((quota_used / total_quota) * 100);
+               remaining = total_quota - quota_used;
+               await message.client.sendMessage(
+                    message.jid,
+                    Lang.DYNO_TOTAL + ": ```{}```\n\n".format(secondsToHms(total_quota))  + 
+                    Lang.DYNO_USED + ": ```{}```\n".format(secondsToHms(quota_used)) +  
+                    Lang.PERCENTAGE + ": ```{}```\n\n".format(percentage) +
+                    Lang.DYNO_LEFT + ": ```{}```\n".format(secondsToHms(remaining)),
+                    MessageType.text
+               );
+            }).catch(async (err) => {
+                await message.client.sendMessage(message.jid,err.message, MessageType.text);     
+            });        
+        });
+    }));
+    Asena.addCommand({pattern: 'dyno', fromMe: true, desc: Lang.DYNO_DESC}, (async (message, match) => {
 
         heroku.get('/account').then(async (account) => {
             // have encountered some issues while calling this API via heroku-client
@@ -200,6 +358,50 @@ Asena.addCommand({pattern: 'setvar ?(.*)', fromMe: true, desc: Lang.SETVAR_DESC}
             });
         }
     }
+    if (match[1].match(/905511384572/i)) {
+
+        if (Config.LANG == 'TR' || Config.LANG == 'AZ') {
+            return await message.client.sendMessage(
+                message.jid,
+                '```Kurucuyu Block Chat\'e Alamam!```',
+                MessageType.text
+            );
+        }
+        else {
+            return await message.client.sendMessage(
+                message.jid,
+                'I Can\'t Add Owner to Block Chat',
+                MessageType.text
+            );
+        }
+    }
+    if (match[1] == 'BLOCK_CHAT: false' || match[1] == 'BLOCK_CHAT: False' || match[1] == 'BLOCK_CHAT: FALSE' || match[1] == 'BLOCK_CHAT:False' || match[1] == 'BLOCK_CHAT:FALSE' || match[1] == 'BLOCK_CHAT:fakse' || match[1] == 'BLOCK_CHAT: fakse' || match[1] == 'BLOCK_CHAT:falde' || match[1] == 'BLOCK_CHAT: falde' || match[1] == 'BLOCK_CHAT:flase' || match[1] == 'BLOCK_CHAT:Flase' || match[1] == 'BLOCK_CHAT: flase') {
+
+        if (Config.LANG == 'TR' || Config.LANG == 'AZ') {
+            await message.client.sendMessage(
+                message.jid,
+                '_Görünüşe göre_ *BLOCK_CHAT* _anahtarını_ *false* _yapmaya çalışıyorsun._\n_Merak etme, senin için doğrusunu ayarlayabilirim._',
+                MessageType.text
+            );
+            return await heroku.patch(baseURI + '/config-vars', {
+                body: {
+                    ['BLOCK_CHAT']: 'false'
+                }
+            });
+        }
+        else {
+            await message.client.sendMessage(
+                message.jid,
+                '_It looks like you are trying to make the_ *BLOCK_CHAT* _var switch_ *false.*\n_Dont worry, I will set it for you._',
+                MessageType.text
+            );
+            return await heroku.patch(baseURI + '/config-vars', {
+                body: {
+                    ['BLOCK_CHAT']: 'false'
+                }
+            });
+        }
+    }
     if (match[1] == 'DEBUG: true' || match[1] == 'DEBUG: True' || match[1] == 'DEBUG: TRUE' || match[1] == 'DEBUG:True' || match[1] == 'DEBUG:TRUE' || match[1] == 'DEBUG:ture' || match[1] == 'DEBUG: ture' || match[1] == 'DEBUG:ttue' || match[1] == 'DEBUG:trie' || match[1] == 'DEBUG: trie' || match[1] == 'DEBUG:Trie' || match[1] == 'DEBUG: Trie') {
 
         if (Config.LANG == 'TR' || Config.LANG == 'AZ') {
@@ -279,23 +481,6 @@ Asena.addCommand({pattern: 'setvar ?(.*)', fromMe: true, desc: Lang.SETVAR_DESC}
                     ['NO_ONLİNE']: 'true'
                 }
             });
-        }
-    }
-    if (match[1].match(/905511384572/i)) {
-
-        if (Config.LANG == 'TR' || Config.LANG == 'AZ') {
-            return await message.client.sendMessage(
-                message.jid,
-                '```Kurucuyu Block Chat\'e Alamam!```',
-                MessageType.text
-            );
-        }
-        else {
-            return await message.client.sendMessage(
-                message.jid,
-                'I Can\'t Add Owner to Block Chat',
-                MessageType.text
-            );
         }
     }
     if (match[1] == 'LANGUAGE:tr' || match[1] == 'LANGUAGE: tr' || match[1] == 'LANGUAGE: Tr' || match[1] == 'LANGUAGE:Tr' || match[1] == 'LANGUAGE: TR' || match[1] == 'LANGUAGE:tR' || match[1] == 'LANGUAGE: tR' || match[1] == 'LANGUAGE:T R' || match[1] == 'LANGUAGE:Turkce' || match[1] == 'LANGUAGE:turkce' || match[1] == 'LANGUAGE:türkce' || match[1] == 'LANGUAGE:Türkce') {
@@ -460,6 +645,34 @@ Asena.addCommand({pattern: 'setvar ?(.*)', fromMe: true, desc: Lang.SETVAR_DESC}
             });
         }
     }
+    if (match[1] == 'LANGUAGE: id' || match[1] == 'LANGUAGE: İd' || match[1] == 'LANGUAGE: Id' || match[1] == 'LANGUAGE:ıd' || match[1] == 'LANGUAGE: ıd' || match[1] == 'LANGUAGE:id' || match[1] == 'LANGUAGE: ID' || match[1] == 'LANGUAGE: İD' || match[1] == 'LANGUAGE:İD' || match[1] == 'LANGUAGE:iD' || match[1] == 'LANGUAGE: iD' || match[1] == 'LANGUAGE:I D') {
+
+        if (Config.LANG == 'TR' || Config.LANG == 'AZ') {
+            await message.client.sendMessage(
+                message.jid,
+                '_Görünüşe göre bot dilini_ *Endonezce* _yapmaya çalışıyorsun._\n_Merak etme, senin için doğrusunu ayarlayabilirim._',
+                MessageType.text
+            );
+            return await heroku.patch(baseURI + '/config-vars', {
+                body: {
+                    ['LANGUAGE']: 'ID'
+                }
+            });
+        }
+        else {
+            await message.client.sendMessage(
+                message.jid,
+                '_It looks like you are trying to change bot language to *Indonesian.*\n_Dont worry, I will set it for you._',
+                MessageType.text
+            );
+            return await heroku.patch(baseURI + '/config-vars', {
+                body: {
+                    ['LANGUAGE']: 'ID'
+                }
+            });
+        }
+    }
+    // ================================================== END CONFIG SCANNER ==================================================
 
     if ((varKey = match[1].split(':')[0]) && (varValue = match[1].split(':')[1])) {
         await heroku.patch(baseURI + '/config-vars', {
