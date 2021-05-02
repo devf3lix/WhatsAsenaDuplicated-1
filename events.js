4b1b1ffaf605aa98e5528b4e651020cee1bec7b8
Baran <57062330+Xenon67@users.noreply.github.com>
2021-05-02 06:06:23 +0300

    Update events.js

--- events.js
+++ events.js
@@ -16,7 +16,7 @@ function addCommand(info, func) {
 
     var infos = {
         fromMe: info['fromMe'] === undefined ? true : info['fromMe'], // Or Sudo
-        public: info['fromMe'] === undefined ? true : info['fromMe'],
+        isOnlyfromMe: info['isOnlyfromMe'] === undefined ? true : info['isOnlyfromMe'],
         onlyGroup: info['onlyGroup'] === undefined ? false : info['onlyGroup'],
         onlyPinned: info['onlyPinned'] === undefined ? false : info['onlyPinned'],
         onlyPm: info['onlyPm'] === undefined ? false : info['onlyPm'],
