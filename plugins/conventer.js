4d62d13f8a917b966c9e385d77421dc75c48e518
Baran <57062330+Xenon67@users.noreply.github.com>
2021-05-02 05:46:34 +0300

    Update conventer.js

--- plugins/conventer.js
+++ plugins/conventer.js
@@ -9,6 +9,9 @@ const fs = require('fs');
 const ffmpeg = require('fluent-ffmpeg');
 const {execFile} = require('child_process');
 const cwebp = require('cwebp-bin');
+const Axios = require('axios')
+const cheerio = require('cheerio')
+const FormData = require('form-data')
 
 const Language = require('../language');
 const Lang = Language.getString('conventer');
@@ -33,6 +36,53 @@ Asena.addCommand({pattern: 'mp4audio', fromMe: true, desc: Lang.MP4TOAUDİO_DESC
     return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
 }));
 
+
+function webp2mp4File(path) {
+    return new Promise(async (resolve, reject) => {
+        const bodyForm = new FormData()
+        bodyForm.append('new-image-url', '')
+        bodyForm.append('new-image', fs.createReadStream(path))
+        await Axios({
+            method: 'post',
+            url: 'https://s6.ezgif.com/webp-to-mp4',
+            data: bodyForm,
+            headers: {
+                'Content-Type': `multipart/form-data boundary=${bodyForm._boundary}`
+            }
+        }).then(async ({ data }) => {
+            const bodyFormThen = new FormData()
+            const $ = cheerio.load(data)
+            const file = $('input[name="file"]').attr('value')
+            const token = $('input[name="token"]').attr('value')
+            const convert = $('input[name="file"]').attr('value')
+            const gotdata = {
+                file: file,
+                token: token,
+                convert: convert
+            }
+            bodyFormThen.append('file', gotdata.file)
+            bodyFormThen.append('token', gotdata.token)
+            bodyFormThen.append('convert', gotdata.convert)
+            await Axios({
+                method: 'post',
+                url: 'https://ezgif.com/webp-to-mp4/' + gotdata.file,
+                data: bodyFormThen,
+                headers: {
+                    'Content-Type': `multipart/form-data boundary=${bodyFormThen._boundary}`
+                }
+            }).then(({ data }) => {
+                const $ = cheerio.load(data)
+                const result = 'https:' + $('div#output > p.outfile > video > source').attr('src')
+                resolve({
+                    status: true,
+                    message: "Made by WhatsAsena",
+                    result: result
+                })
+            }).catch(reject)
+        }).catch(reject)
+    })
+}
+
 Asena.addCommand({pattern: 'imagesticker', fromMe: true, desc: Lang.STİCKER_DESC}, (async (message, match) => {    
     if (message.reply_message === false) return await message.sendMessage(Lang.STİCKER_NEEDREPLY);
     var downloading = await message.client.sendMessage(message.jid,Lang.STİCKER,MessageType.text);
@@ -52,3 +102,26 @@ Asena.addCommand({pattern: 'imagesticker', fromMe: true, desc: Lang.STİCKER_DES
         });
     return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
 }));
+const een = "Animasyonlu stickerleri videoya çevirir."
+Asena.addCommand({pattern: 'videosticker$', desc: een}, (async (message, match) => {
+    if (message.reply_message === false) return await message.sendMessage(Lang.STİCKER_NEEDREPLY);
+
+    const savedFilename = await message.client.downloadAndSaveMediaMessage({
+        key: {
+            remoteJid: message.reply_message.jid,
+            id: message.reply_message.id
+        },
+        message: message.reply_message.data.quotedMessage
+    });
+    await webp2mp4File(savedFilename).then(async (rest) => {
+        await Axios({ method: "GET", url: rest.result, responseType: "stream"}).then(({ data }) => {
+            const saving = data.pipe(fs.createWriteStream('/root/WhatsAsenaDuplicated/stweb.mp4'))
+            saving.on("finish", () => {
+                await message.client.sendMessage(message.jid, fs.readFileSync('/root/WhatsAsenaDuplicated/stweb.mp4'), MessageType.video, { mimetype: Mimetype.mp4, caption: 'Made by WhatsAsena', quoted: message.data })
+                if (fs.existsSync(savedFilename)) fs.unlinkSync(savedFilename)
+                if (fs.existsSync('/root/WhatsAsenaDuplicated/stweb.mp4')) fs.unlinkSync('/root/WhatsAsenaDuplicated/stweb.mp4')
+            })
+        })
+    })
+}));
+       
