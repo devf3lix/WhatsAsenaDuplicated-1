4aaddc08073102401df7b26898281ae3f8b310fd
Baran <57062330+Xenon67@users.noreply.github.com>
2021-05-02 20:54:33 +0300

    Update permit.js

--- plugins/permit.js
+++ plugins/permit.js
@@ -5,11 +5,11 @@ const Config = require('../config');
 const pmpermit = require('./plugins/pmpermit');
 const ds = "PM Permit iznini açar."
 const dds = "PM Permit iznini kapatır."
-Asena.addCommand({pattern: 'approve', desc: ds, onlyPm: true}, (async (message, match) => { 
+Asena.addCommand({pattern: 'approve$', desc: ds, onlyPm: true}, (async (message, match) => { 
     pmpermit.permitacton(message.jid.split("@")[0])
     await message.client.sendMessage(message.jid,'PM İzni Verildi!', MessageType.text);
 }));
-Asena.addCommand({pattern: 'disapprove', desc: dss, onlyPm: true}, (async (message, match) => { 
+Asena.addCommand({pattern: 'disapprove$', desc: dss, onlyPm: true}, (async (message, match) => { 
     pmpermit.nopermitacton(message.jid.split("@")[0])
     await message.client.sendMessage(message.jid,'PM İzni Kaldırıldı!', MessageType.text);
 }));
