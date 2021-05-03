63527185b1b021b6a6e48be63f6e8b236359658b
Baran <57062330+Xenon67@users.noreply.github.com>
2021-05-03 12:56:53 +0300

    Update public.js

--- plugins/public.js
+++ plugins/public.js
@@ -70,8 +70,7 @@ Asena.addCommand({on: 'text', fromMe: true, deleteCommand: false}, (async (messa
 }));
 Asena.addCommand({on: 'text', fromMe: false, deleteCommand: false}, (async (message, match) => {
     if (config.ANTİLİNK == 'true') {
-        let regex = new RegExp('http');
-        if (message.message.match(regex)) {
+        if (message.message.includes('http')) {
             isLink.situation.parts = true
             await message.client.sendMessage(message.jid,'.antilink \n\n*Links Are Unallowed!*', MessageType.text, { quoted: message.data })
         }
