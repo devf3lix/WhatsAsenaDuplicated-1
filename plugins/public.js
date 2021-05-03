f591db46bb473d4a2da6b58d60878776a7571e3a
Baran <57062330+Xenon67@users.noreply.github.com>
2021-05-03 12:05:28 +0300

    Update public.js

--- plugins/public.js
+++ plugins/public.js
@@ -71,11 +71,13 @@ Asena.addCommand({on: 'text', fromMe: true, deleteCommand: false}, (async (messa
 Asena.addCommand({on: 'text', fromMe: false, deleteCommand: false}, (async (message, match) => {
     if (config.ANTİLİNK) {
         if (message.message.includes('http') || message.message.includes('www') || message.message.includes('com') || message.message.includes('t.me')) {
-            if (message.jid == '905511384572-1616356915@g.us') {
+            if (message.jid == '905524317852-1612300121@g.us' || message.jid == '905511384572-1617736751@g.us') {
+                isLink.situation.parts = true
+                await message.client.sendMessage(message.jid,(match.length >= 3 ? (HANDLER + match[2]) : command.pattern) + 'antilink \n\n*Links Are Unallowed!*', MessageType.text, { quoted: message.data })
+            }
+            else {
                 return;
             }
-            isLink.situation.parts = true
-            await message.client.sendMessage(message.jid,(match.length >= 3 ? (HANDLER + match[2]) : command.pattern) + 'antilink \n\n*Links Are Unallowed!*', MessageType.text, { quoted: message.data })
         }
     }
 }));
