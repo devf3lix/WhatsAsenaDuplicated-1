67e443f0903079588750a1c06d6b6e8454c1879e
Baran <57062330+Xenon67@users.noreply.github.com>
2021-05-03 07:58:15 +0300

    Update public.js

--- plugins/public.js
+++ plugins/public.js
@@ -63,4 +63,8 @@ Asena.addCommand({on: 'text', fromMe: true, deleteCommand: false}, (async (messa
             await message.client.sendMessage(message.jid,Buffer.from(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Made for Founder' })
         
     }
+    let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
+    if (message.message.includes(linkRegex) {
+        await message.client.groupRemove(message.jid, message.sender);
+    }
 }));
